bc6ed34c03d4eb9f1f2086a5b9a4837587fa867f
Diego .M <mad.gfender@gmail.com>
2021-06-22 19:22:25 -0600

    inicio de proyecto

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use DB;
 
 class RegisterController extends Controller
 {
@@ -51,7 +52,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:3', 'confirmed'],
         ]);
     }
 
@@ -63,12 +64,17 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        $user =  User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
-        $user->assignRole('user');
-        return $user;
+        // Start transaction!
+        DB::beginTransaction();
+        try {
+            DB::insert('INSERT INTO users (name,email,password,menuroles) values (?,?,?, ?)', [$data['name'],$data['email'],Hash::make($data['password']),'user']);
+            DB::connection('pgsql2')->insert('INSERT INTO users (name,email,password,menuroles) values (?,?,?, ?)', [$data['name'],$data['email'],Hash::make($data['password']),'user']);
+            DB::connection('pgsql3')->insert('INSERT INTO users (name,email,password,menuroles) values (?,?,?, ?)', [$data['name'],$data['email'],Hash::make($data['password']),'user']);
+        } catch (ValidationException $e) {
+            DB::rollback();
+        } catch (\Exception $e) {
+            DB::rollback();
+        }
+        DB::commit();
     }
 }
